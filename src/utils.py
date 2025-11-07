db1930b3709547109234a583b11721602a33206f
rita <machaandmandula000@gmail.com>
2025-11-07 09:12:36 +0100

    Add utils

---
+++ src/utils.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+import yaml
+
+def load_config():
+    base_dir = Path(__file__).resolve().parent.parent
+    config_path = base_dir / 'config.yaml'
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config, base_dir
