81feec8529db1662d7bf04044c336d2c5283a744
rita <machaandmandula000@gmail.com>
2025-11-24 13:26:01 +0100

    add new functions

--- src/package/__init__.py
+++ src/package/__init__.py
@@ -1,3 +1,5 @@
 from .nd_to_tiff import convert_nd2_to_tiff
 from .tiff_viewer import view_tiff_directory, view_masks_napari 
 from .mask_maker import create_masks
+from .cellpose_trainer import train_cellpose_model
+from .intensity_measurer import extract_intensity_measurements, process_image_batch
