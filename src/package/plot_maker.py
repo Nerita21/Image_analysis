31d14028b6072dfbc1699b578d39de69e29f8d15
rita <machaandmandula000@gmail.com>
2025-12-01 21:42:21 +0100

    upload files

---
+++ src/package/plot_maker.py
@@ -0,0 +1,99 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from pathlib import Path
+
+def plot_intensity_cyto(csv_file: Path, output_dir: Path):
+    """
+    Generate and save intensity distribution plots from CSV data.
+
+    Args:
+        csv_file (Path): Path to the CSV file containing intensity measurements.
+        output_dir (Path): Directory to save the generated plots.
+    """
+    # Load data
+    data = pd.read_csv(csv_file)
+
+    # Create output directory if it doesn't exist
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # Plot settings
+    sns.set_style(style="whitegrid")
+    sns.set_palette("Set2")
+
+   # Generate pboxplot for intensity distributions between samples
+    plt.figure(figsize=(10, 6))                                                         
+    sns.barplot(x='sample_name', y='cytoplasmic_intensity_mean', palette="deep", data=data)
+    plt.title('Cytoplasmic ntensity Distribution by Sample')
+    plt.xlabel('Sample')
+    plt.ylabel('Intensity')
+    plt.xticks(rotation=0)
+    plt.tight_layout()
+    plot_path = output_dir / "Cytoplasm_intensity_distribution_boxplot.png"
+    plt.savefig(plot_path)
+    plt.close()
+    print(f"Plot saved to {plot_path}")
+
+def plot_intensity_nuclei(csv_file: Path, output_dir: Path):
+    """
+    Generate and save intensity distribution plots from CSV data.
+
+    Args:
+        csv_file (Path): Path to the CSV file containing intensity measurements.
+        output_dir (Path): Directory to save the generated plots.
+    """
+    # Load data
+    data = pd.read_csv(csv_file)
+
+    # Create output directory if it doesn't exist
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # Plot settings
+    sns.set_style(style="whitegrid")
+    sns.set_palette("Set2")
+
+
+   # Generate pboxplot for intensity distributions between samples
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='sample_name', y='nuclear_intensity_mean', palette="deep", data=data)
+    plt.title('Nuclear intensity Distribution by Sample')
+    plt.xlabel('Sample')
+    plt.ylabel('Intensity')
+    plt.xticks(rotation=0)
+    plt.tight_layout()
+    plot_path = output_dir / "Nuclear_intensity_distribution_boxplot.png"
+    plt.savefig(plot_path)
+    plt.close()
+    print(f"Plot saved to {plot_path}")
+
+def plot_intensity_ratio(csv_file: Path, output_dir: Path):
+    """
+    Generate and save intensity distribution plots from CSV data.
+
+    Args:
+        csv_file (Path): Path to the CSV file containing intensity measurements.
+        output_dir (Path): Directory to save the generated plots.
+    """
+    # Load data
+    data = pd.read_csv(csv_file)
+
+    # Create output directory if it doesn't exist
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # Plot settings
+    sns.set_style(style="whitegrid")
+    sns.set_palette("Set2")
+
+
+   # Generate pboxplot for intensity distributions between samples
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='sample_name', y='nuclear_to_cytoplasmic_ratio', palette="deep", data=data)
+    plt.title('Intensity Ratio between nuclei and cytoplasm by Sample')
+    plt.xlabel('Sample')
+    plt.ylabel('Ratio')
+    plt.xticks(rotation=0)
+    plt.tight_layout()
+    plot_path = output_dir / "intensity_ratio_barplot.png"
+    plt.savefig(plot_path)
+    plt.close()
+    print(f"Plot saved to {plot_path}")
