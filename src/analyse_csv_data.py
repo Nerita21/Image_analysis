bfd4926799234ea963bd4ca6f9533182009aabc1
rita <machaandmandula000@gmail.com>
2025-12-21 14:33:19 +0100

    Includes hardcode filenames as examples. Please adapt as needed.

--- src/analyse_csv_data.py
+++ src/analyse_csv_data.py
@@ -1,3 +1,4 @@
+# %% This script analyses CSV data containing intensity measurements and creates plots.
 # %% Import necessary libraries
 import pandas as pd
 from utils import load_config
@@ -8,13 +9,12 @@ config, base_dir = load_config()
 
 # %% 
 data_dir = base_dir / config["data"]["csv_dir"]
+plot_dir = base_dir / config["data"]["plots_dir"]
+# %% Load data (example for knockdown data)
 data = pd.read_csv(data_dir / "intensity_measurements_h1792KD.csv")
-output_dir = data_dir / "plot_KD"
+output_dir = plot_dir / "plot_KD"
 
 # %% Create plots
 plot_intensity_cyto(data_dir / "intensity_measurements_h1792KD.csv", output_dir)
 plot_intensity_nuclei(data_dir / "intensity_measurements_h1792KD.csv", output_dir)
 plot_intensity_ratio(data_dir / "intensity_measurements_h1792KD.csv", output_dir)
-
-
-# %%
--- src/napari_examiner.py
+++ src/napari_examiner.py
@@ -2,6 +2,7 @@
 import napari
 from utils import load_config
 from package import view_tiff_directory, view_masks_napari
+from pathlib import Path
 # %% Load configuration
 config, base_dir = load_config()
 
@@ -15,11 +16,22 @@ corrected_masks_dir = base_dir / config["data"]["corrected_masks_dir"]
 # %% View masks in Napari (for manual correction)
 view_masks_napari(raw_masks_dir, raw_tiff_image_dir)
 
-# %%
+# %% Paths for sample TIFF directories
 ctrl_dir = sample_tiff_masks_dir/"sample_ctrl"
 ctrl_treat_dir = sample_tiff_masks_dir/"sample_treated"
 saber_dir = sample_tiff_masks_dir/"sample_saber_ctrl"
 saber_treat_dir = sample_tiff_masks_dir/"sample_saber_treated"
+
+# %% Paths for knockdown data
+kd_hela_dir_ctrl = Path(base_dir / "data/knockdown/masks/Ctrl")
+kd_h1792_dir_ctrl = Path(base_dir / "data/knockdown/masks_h1792/Ctrl")
+hela_dir_ctrl = Path(base_dir / "data/knockdown/tiff/Ctrl")
+h1792_dir_ctrl = Path(base_dir / "data/knockdown/tiff_h1792/Ctrl")
+
+# %% View knockdown data in Napari
+view_masks_napari(kd_hela_dir_ctrl, hela_dir_ctrl)
+# %%
+view_masks_napari(kd_h1792_dir_ctrl, h1792_dir_ctrl)
 # %% View sample TIFF directory in Napari
 view_tiff_directory(ctrl_dir)
 # %%
